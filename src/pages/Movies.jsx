c53efac597cb88aa877a7d699751f85a69f00951
hadee2021 <tpghks9920@naver.com>
2022-07-27 20:43:27 +0900

    clear

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -28,64 +28,11 @@ const Movies = () => {
     ...upComingMovies.results
   ])
 
-  // const [sortShow, setSortShow] = useState(false)
-  // const sortSwitch = () => {
-  //   setSortShow(!sortShow)
-  // }
-
-  // const sortAsc = (sortKey) => {
-  //   const newItem = [...pageItems]
-  //   newItem.sort((a, b) => {
-  //     if(a[sortKey] > b[sortKey]) {
-  //       return 1
-  //     }
-  //     if(a[sortKey] < b[sortKey]) {
-  //       return -1
-  //     }
-  //     return 0
-  //   })
-  //   setPageItems(newItem)
-  // }
-  // const sortDesc = (sortKey) => {
-  //   const newItem = [...pageItems]
-  //   newItem.sort((a, b) => {
-  //     if(a[sortKey] < b[sortKey]) {
-  //       return 1
-  //     }
-  //     if(a[sortKey] > b[sortKey]) {
-  //       return -1
-  //     }
-  //     return 0
-  //   })
-  //   setPageItems(newItem)
-  // }
-
   if(loading) {
     return <ClipLoader color="#ffff" loading={loading}  size={150} />
   }
   return (
     <div className="center movies-page">
-      {/* <div className="movies-page-menu">
-        <div className="sort" onClick={sortSwitch}>
-          Sort
-          {
-            sortShow 
-            ? <KeyboardArrowUpIcon /> 
-            : <KeyboardArrowDownIcon />
-          }
-        </div>
-        {
-          sortShow &&
-          <ul className="sort-ul">
-            <li onClick={() => sortAsc("popularity")}>Popularity(Asc)</li>
-            <li onClick={() => sortDesc("popularity")}>Popularity(Desc)</li>
-            <li onClick={() => sortAsc("release_date")}>Release Day(Asc)</li>
-            <li onClick={() => sortDesc("release_date")}>Release Day(Desc)</li>
-            <li onClick={() => sortAsc("vote_average")}>Vote(Asc)</li>
-            <li onClick={() => sortDesc("vote_average")}>Vote(Desc)</li>
-          </ul>
-        }
-      </div> */}
       <Sort 
         pageItems={pageItems} 
         setPageItems={setPageItems}
