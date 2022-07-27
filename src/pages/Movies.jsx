a031266ec087328da79d3d3e8955fa299a33fe56
hadee2021 <tpghks9920@naver.com>
2022-07-27 20:31:59 +0900

    FIX:clear-code

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -4,8 +4,6 @@ import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp'
 import { useDispatch, useSelector } from 'react-redux'
 import { movieAction } from '../redux/actions/movieAction'
 import ClipLoader from 'react-spinners/ClipLoader'
-// import MovieCard from '../components/MovieCard'
-// import Paging from '../components/Paging'
 import FourPaging from '../components/FourPaging'
 
 const Movies = () => {
@@ -23,28 +21,11 @@ const Movies = () => {
     dispatch(movieAction.getMovies())
   },[])
 
-  // const [currentPage, setCurrentPage] = useState(1)
-  // const handlePageChange = (currentPage) => {
-  //   setCurrentPage(currentPage)
-  // }
-
   const [pageItems, setPageItems] = useState([
     ...popularMovies.results,
     ...topRatedMovies.results,
     ...upComingMovies.results
   ])
-  // const [currentItems, setCurrentItems] = useState([])
-  // const [count, setCount] = useState(0)
-  // const [indexOfLastItem, setIndexOfLastItem] = useState(0)
-  // const [indexOfFirstItem, setIndexOfFirstItem] = useState(0)
-  
-  // useEffect(() => {
-  //   setIndexOfLastItem(4 * currentPage)
-  //   setIndexOfFirstItem(4 * (currentPage - 1) + 1)
-  //   setCurrentItems(pageItems?.slice(indexOfFirstItem - 1, indexOfLastItem))
-  //   setCount(pageItems?.length)
-  // },[currentPage, pageItems, indexOfFirstItem])
-
 
   const [sortShow, setSortShow] = useState(false)
   const sortSwitch = () => {
@@ -104,16 +85,6 @@ const Movies = () => {
           </ul>
         }
       </div>
-      {/* <div className='movies-page-movie'>
-        {currentItems?.map((item) => (
-          <MovieCard item={item} key={item.id}/>
-        ))}
-        <Paging 
-          currentPage={currentPage}
-          handlePageChange={handlePageChange}
-          count={count}
-        />
-      </div> */}
       <FourPaging pageItems={pageItems}/>
     </div>
   )
